6c3a4edc55601a13fef0a475b610ae965e0994d5
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-12 21:19:00 +0200

    added posts navigation

--- index.php
+++ index.php
@@ -33,13 +33,7 @@
                                 }
                             }
                             ?>
-		                    <nav class="navigation pagination">
-		                        <h2 class="screen-reader-text">Inläggsnavigering</h2>
-		                        <a class="prev page-numbers" href="">Föregående</a>
-		                        <span class="page-numbers current">1</span>
-		                        <a class="page-numbers" href="">2</a>
-		                        <a class="next page-numbers" href="">Nästa</a>
-		                    </nav>
+		                    <?php the_posts_pagination(); ?>
 		                </div>
 		                <aside id="secondary" class="col-xs-12 col-md-3">
 		                    <div id="sidebar">
