26ece592ab22dd3bc63876712fb4ae37a3c4b196
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-12 18:32:14 +0200

    created template

---
+++ page-undersida-4.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-md-6">
+                    <?php
+                    if (have_posts()) {
+                        while (have_posts()) {
+                            the_post();
+                            the_title('<h1>', '</h1>');
+                            the_content();
+                        }
+                    }
+                    ?>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-6">
+                    <?php the_post_thumbnail('medium_large'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
