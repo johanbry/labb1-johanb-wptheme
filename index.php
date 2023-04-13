eb0172db0bb4f33d3effafb418560968b93952be
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-13 17:19:55 +0200

    use template parts

--- index.php
+++ index.php
@@ -6,34 +6,13 @@
 		                <div id="primary" class="col-xs-12 col-md-9">
 		                    <h1><?php wp_title(''); ?></h1>
 
-		                    <?php
-                            if (have_posts()) {
+		                    <?php if (have_posts()) {
                                 while (have_posts()) {
                                     the_post();
-                            ?>
-		                            <article>
-		                                <img src="<?php the_post_thumbnail_url('large'); ?>">
-		                                <h2>
-		                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		                                </h2>
-		                                <ul class="meta">
-		                                    <li>
-		                                        <i class="fa fa-calendar"></i><?php the_time('j F, Y'); ?>
-		                                    </li>
-		                                    <li>
-		                                        <i class="fa fa-user"></i> <?php the_author_posts_link(); ?>
-		                                    </li>
-		                                    <li>
-		                                        <i class="fa fa-tag"></i> <?php the_category(', '); ?>
-		                                    </li>
-		                                </ul>
-		                                <?php the_excerpt(); ?>
-		                            </article>
-		                    <?php
+                                    get_template_part('template-parts/content', 'excerpt');
                                 }
                             }
-                            ?>
-		                    <?php the_posts_pagination(); ?>
+                            the_posts_pagination(); ?>
 		                </div>
 		                <aside id="secondary" class="col-xs-12 col-md-3">
 		                    <?php get_sidebar(); ?>
