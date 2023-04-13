bad424188446aba7d8bbcfb0f16b07daa63b967b
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-13 18:44:21 +0200

    template created

---
+++ author.php
@@ -0,0 +1,25 @@
+		<?php get_header(); ?>
+		<main>
+		    <section>
+		        <div class="container">
+		            <div class="row">
+		                <div id="primary" class="col-xs-12 col-md-9">
+
+		                    <h1><?php the_author(); ?></h1>
+
+		                    <?php if (have_posts()) {
+                                while (have_posts()) {
+                                    the_post();
+                                    get_template_part('template-parts/content', 'excerpt');
+                                }
+                            }
+                            the_posts_pagination(); ?>
+		                </div>
+		                <aside id="secondary" class="col-xs-12 col-md-3">
+		                    <?php get_sidebar(); ?>
+		                </aside>
+		            </div>
+		        </div>
+		    </section>
+		</main>
+		<?php get_footer(); ?>
