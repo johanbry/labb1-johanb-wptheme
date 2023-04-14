ca2c9795d1ee43ce6e585ba2517f3f95aadf17dd
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-14 15:17:23 +0200

    simplified with just the basic loop

--- index.php
+++ index.php
@@ -3,20 +3,15 @@
 		    <section>
 		        <div class="container">
 		            <div class="row">
-		                <div id="primary" class="col-xs-12 col-md-9">
-		                    <h1><?php wp_title(''); ?></h1>
-
+		                <div id="primary" class="col-xs-12">
 		                    <?php if (have_posts()) {
                                 while (have_posts()) {
+                                    the_title('<h1>', '</h1>');
                                     the_post();
-                                    get_template_part('template-parts/content', 'excerpt');
+                                    the_content();
                                 }
-                            }
-                            the_posts_pagination(); ?>
+                            } ?>
 		                </div>
-		                <aside id="secondary" class="col-xs-12 col-md-3">
-		                    <?php get_sidebar(); ?>
-		                </aside>
 		            </div>
 		        </div>
 		    </section>
