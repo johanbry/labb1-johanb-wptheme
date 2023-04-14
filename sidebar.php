2410a802780d2c04f35e330d670fda44371d1d32
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-14 14:51:45 +0200

    replaced hardcoded search with widget

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,15 @@
 <div id="sidebar">
-    <ul>
-        <li>
-            <?php get_search_form(); ?>
-        </li>
-    </ul>
+    <?php dynamic_sidebar('aside-section-1'); ?>
     <ul role="navigation">
         <li class="pagenav">
-            <?php dynamic_sidebar('aside-section-1'); ?>
+            <?php dynamic_sidebar('aside-section-2'); ?>
         </li>
         <li>
-            <?php dynamic_sidebar('aside-section-2'); ?>
+            <?php dynamic_sidebar('aside-section-3'); ?>
 
         </li>
         <li class="categories">
-            <?php dynamic_sidebar('aside-section-3'); ?>
+            <?php dynamic_sidebar('aside-section-4'); ?>
         </li>
     </ul>
 </div>
