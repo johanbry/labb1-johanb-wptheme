79f9e9f07fa150d6041ed6eec207fb7693929252
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-17 11:57:14 +0200

    added comments

--- archive.php
+++ archive.php
@@ -4,15 +4,21 @@
 				<div class="container">
 					<div class="row">
 						<div id="primary" class="col-xs-12 col-md-9">
-
+							<?php /* En egenskriven funktion i functions php som formaterar rubrik. */ ?>
 							<h1><?= get_archive_title(); ?></h1>
-
+							<?php
+							/*
+							The Loop som skriver ut alla matchande inlägg.
+							*/
+							?>
 							<?php if (have_posts()) {
 								while (have_posts()) {
 									the_post();
+									/* Använd template part som skriver ut en sammanfattning av inlägget. */
 									get_template_part('template-parts/content', 'excerpt');
 								}
 							}
+							/* Sidpaginering om inte alla inlägg får plats på en sida. */
 							the_posts_pagination(); ?>
 						</div>
 						<aside id="secondary" class="col-xs-12 col-md-3">
