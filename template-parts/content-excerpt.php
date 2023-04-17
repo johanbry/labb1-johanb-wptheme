59c1e9778140384ee74f18504dafa83cbd099ffa
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-17 11:42:57 +0200

    added comments

--- template-parts/content-excerpt.php
+++ template-parts/content-excerpt.php
@@ -5,10 +5,14 @@
 ?>
 
 <article>
+    <?php /* Hämta adressen till den utvalda bilden i storlek large. */ ?>
     <img src="<?php the_post_thumbnail_url('large'); ?>">
     <h2>
+        <?php /* Skriv ut titeln på inlägget och gör den klickbar för att gå till det enskilda inlägget. */ ?>
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h2>
+    <?php /* Använd template part som skriver ut meta-info för inlägget */ ?>
     <?php get_template_part('template-parts/post', 'meta'); ?>
+    <?php /* Skriv ut utdraget för inlägget. */ ?>
     <?php the_excerpt(); ?>
 </article>
