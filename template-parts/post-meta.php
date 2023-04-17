7f7bbd319e2fbcd5091253215f292dd39b71f2a7
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-17 11:50:37 +0200

    added comments

--- template-parts/post-meta.php
+++ template-parts/post-meta.php
@@ -9,12 +9,15 @@
 
 <ul class="meta">
     <li>
+        <?php /* Skriv ut när inlägget skapats enligt angivet format. */ ?>
         <i class="fa fa-calendar"></i><?php the_time('j F, Y'); ?>
     </li>
     <li>
+        <?php /* Skriv ut länk till sida med författarens alla posts. */ ?>
         <i class="fa fa-user"></i> <?php the_author_posts_link(); ?>
     </li>
     <li>
+        <?php /* Skriv ut länkar till alla kategorier för inlägget (separerade med kommatecken). */ ?>
         <i class="fa fa-tag"></i> <?php the_category(', '); ?>
     </li>
 </ul>
