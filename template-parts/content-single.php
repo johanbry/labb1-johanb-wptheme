1de3df459ba3364dd6366fa54036b8763f880618
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-17 11:46:54 +0200

    added comments

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -5,10 +5,14 @@
 ?>
 
 <article>
+    <?php /* Hämta adressen till den utvalda bilden i storlek large */ ?>
     <img src="<?php the_post_thumbnail_url('large'); ?>">
     <h2>
+        <?php /* Använd template part som skriver ut meta-info för inlägget */ ?>
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h2>
+    <?php /* Använd template part som skriver ut meta-info för inlägget */ ?>
     <?php get_template_part('template-parts/post', 'meta'); ?>
+    <?php /* Skriv ut inläggets innehåll. */ ?>
     <?php the_content(); ?>
 </article>
